d57f9da8b55e56fbbc336e426f900b8beb6bed40
hardipsolanki22 <hardipsolanki2224@gmail.com>
2025-01-25 22:02:19 +0530

    add 5

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser'
 const app = express()
 
 app.use(cors({
-    origin: ["https://the-blog-client.vercel.app/", "http://localhost:5173"],
+    origin: [process.env.REQUEST_URL, "http://localhost:5173"],
     credentials: true,
 }));
 
